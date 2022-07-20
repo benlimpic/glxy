6db66fc64c7f6baeb681fa8ab39391a74aa206c3
init.defaultBranch <benlimpic@gmail.com>
2022-07-20 16:38:31 -0700

    view galaxy css

--- client/src/components/GalaxyBuild.js
+++ client/src/components/GalaxyBuild.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import {useEffect} from 'react';
 import './GalaxyBuild.css'
 function GalaxyBuild({setSelectProject, project, tasks, subtasks}) {
 
@@ -10,7 +10,7 @@ function GalaxyBuild({setSelectProject, project, tasks, subtasks}) {
 
   const ABC = ["a", "b", "c", "d", "e", "f", "g", "h" ]
   
-  return  <div>
+  return  <div classname="galaxyShow">
             <div className="galaxy">
               <div id="universe" className="scale-stretched">
                 <div id="galaxy">
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
