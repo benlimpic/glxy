cf70312a0ef2ba92a88a10a299e6e008a359319f
init.defaultBranch <benlimpic@gmail.com>
2022-08-01 21:43:59 -0700

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000/"
+    origins "https://glxy-build.herokuapp.com/"
 
     resource "*",
       headers: :any,
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
 
 
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
   get '/me', to: "users#me"
   post '/signup', to: "users#signup"
   post '/login', to: "sessions#login"
