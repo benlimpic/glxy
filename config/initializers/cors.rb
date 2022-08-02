a15d17296bd85d15e51275321f9f5e2bb69bb79d
init.defaultBranch <benlimpic@gmail.com>
2022-08-01 22:00:59 -0700

    x

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,9 +8,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
+
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
   end
 end
+
