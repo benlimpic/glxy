7b5fec6701070d387d0ee5a64f28916b4dc18643
init.defaultBranch <benlimpic@gmail.com>
2022-10-04 17:10:05 -0700

    root issue fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,21 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
-  # rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
-  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
-  # private
+  private
 
-  # def render_invalid(err)
-  #   render json: {errors: err.record.errors.full_messages}, status: 422
-  # end
+  def render_invalid(err)
+    render json: {errors: err.record.errors.full_messages}, status: 422
+  end
 
-  # def render_not_found(err)
-  #   render json: {errors: err.errors.full_messages}, status: 404
-  # end
+  def render_not_found(err)
+    render json: {errors: err.errors.full_messages}, status: 404
+  end
 
-  # def render_unauthorized(err)
-  #   render json: {errors: "Not Authorized"}, status: 401
-  # end
+  def render_unauthorized(err)
+    render json: {errors: "Not Authorized"}, status: 401
+  end
 
 end
