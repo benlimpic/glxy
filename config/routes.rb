49680e96bace39c830bdbbf6caaee3bd0b3d2ba3
init.defaultBranch <benlimpic@gmail.com>
2022-11-05 16:55:32 -0700

    home

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :subtasks
 
 
-
+  get '/', to: 'users#index'
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
   get '/me', to: "users#me"
