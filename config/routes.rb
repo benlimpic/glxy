46fe83791c5a5c4c3e1f987555cfeb851b603923
init.defaultBranch <benlimpic@gmail.com>
2022-11-06 12:58:36 -0800

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  root '"users#me"'
+  root "users#me"
   resources :users
   resources :projects
   resources :tasks
   resources :subtasks
 
 
-  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   get '/me', to: "users#me"
   post '/signup', to: "users#signup"
   post '/login', to: "sessions#login"
