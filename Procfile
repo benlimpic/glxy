d7a634b4d03ccdf962048f4991dc2dd08105b2bc
init.defaultBranch <benlimpic@gmail.com>
2022-11-06 09:22:07 -0800

    fix

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec rackup config.ru -p $PORT
-release: bin/rake db:migrate db:seed
+release: rails db:migrate db:seed
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'fallback#index'
+  root '"users#me"'
   resources :users
   resources :projects
   resources :tasks
