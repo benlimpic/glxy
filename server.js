13d81d7d2d718db55140cc5e59811d6ea1621103
init.defaultBranch <benlimpic@gmail.com>
2022-11-06 13:52:08 -0800

    fix

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join('build', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('build'));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join('build', 'index.html'));
+//   });
+// }
